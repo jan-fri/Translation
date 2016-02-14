f84725dfbcc2930ed9422da3395efcd6bc5a7af7
janfri <janfri@wp.pl>
2016-02-14 23:08:29 +0100

    master branch

---
+++ Translation/Translation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translation
+{
+    enum SymbolType
+    {
+        leftParenthesis,
+        rightParenthesis,
+        operation,
+        number,
+        floatingNumber,
+        identifier        
+    }
+
+    class Program
+    {
+        private SymbolType _lecsicalSymbolType;
+        private string _line;
+        private char _symbol;
+
+        private void ReadFromFile(string filePath)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(filePath);
+            while ((_line = file.ReadLine()) != null)
+            {
+                AnalizeLine();
+            }
+
+            file.Close();
+        }
+        
+        private void AnalizeLine()
+        {
+            foreach (char _symbol in _line)
+            {
+                AnalizeLecsicalSymbolType(); 
+            }
+        }
+
+        private void AnalizeLecsicalSymbolType()
+        {
+            switch (_symbol)
+            {
+                default:
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
