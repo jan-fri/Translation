51ff4823fb94559f5237659404c6573076385433
janfri <janfri@wp.pl>
2016-02-17 23:59:30 +0100

    dev

--- Translation/Translation/Program.cs
+++ Translation/Translation/Program.cs
@@ -11,46 +11,135 @@ namespace Translation
         leftParenthesis,
         rightParenthesis,
         operation,
-        number,
+        integer,
         floatingNumber,
-        identifier        
+        identifier,
+        whiteSpace,
+        newLine
     }
 
-    class Program
+    public class Program
     {
+        private int _counter;
         private SymbolType _lecsicalSymbolType;
         private string _line;
         private char _symbol;
+        private StringBuilder _identifier;
+        private StringBuilder _integer;
+        private StringBuilder _floatingNumber;
+
 
         private void ReadFromFile(string filePath)
         {
             System.IO.StreamReader file = new System.IO.StreamReader(filePath);
             while ((_line = file.ReadLine()) != null)
             {
-                AnalizeLine();
+                AnalizeLecsicalSymbolType();
             }
 
             file.Close();
         }
-        
-        private void AnalizeLine()
+
+        //private void AnalizeLine()
+        //{
+        //    foreach (char _symbol in _line)
+        //    {
+        //        AnalizeLecsicalSymbolType(); 
+        //    }
+        //}
+        //private void GetInteger()
+        //{
+        //    _integer = new StringBuilder();
+        //    _integer.Append(_symbol);
+
+        //    if (++_counter <= _line.Length - 1)
+        //    {
+        //        _symbol = _line[_counter];
+        //        if (char.IsDigit(_symbol))
+        //        {
+        //            while (char.IsDigit(_symbol))
+        //            {
+        //                _integer.Append(_symbol);
+        //                _counter++;
+        //                _symbol = _line[_counter];
+        //            }
+        //        }
+        //        else
+        //            _symbol = _line[_counter--];
+        //    }
+            
+        //}
+
+        private void GetIdentifier()
         {
-            foreach (char _symbol in _line)
+            _identifier = new StringBuilder();
+            _identifier.Append(_symbol);
+
+            if (++_counter <= _line.Length - 1)
             {
-                AnalizeLecsicalSymbolType(); 
+                _symbol = _line[_counter];
+                if (char.IsLetter(_symbol) || char.IsDigit(_symbol))
+                {
+                    while (char.IsLetter(_symbol) || char.IsDigit(_symbol))
+                    {
+                        _identifier.Append(_symbol);
+                        _counter++;
+                        _symbol = _line[_counter];
+                    }
+                }
+                _symbol = _line[_counter--];
             }
+            
         }
+
 
         private void AnalizeLecsicalSymbolType()
         {
-            switch (_symbol)
+            for (_counter = 0; _counter < _line.Length; _counter++)
             {
-                default:
+                _symbol = _line[_counter];
+
+                if (_symbol == '(')
+                {
+                    _lecsicalSymbolType = SymbolType.leftParenthesis;
+                    Console.WriteLine("{0}: {1}", _lecsicalSymbolType, _symbol);
+                }
+                else if (_symbol == ')')
+                {
+                    _lecsicalSymbolType = SymbolType.rightParenthesis;
+                    Console.WriteLine("{0}: {1}", _lecsicalSymbolType, _symbol);
+                }
+                else if (_symbol == '*' || _symbol == '+' || _symbol == '/' || _symbol == '-')
+                {
+                    _lecsicalSymbolType = SymbolType.operation;
+                    Console.WriteLine("{0}: {1}", _lecsicalSymbolType, _symbol);
+                }
+                else if (char.IsWhiteSpace(_symbol))
+                {
+                    _lecsicalSymbolType = SymbolType.whiteSpace;
+                    Console.WriteLine("{0}: {1}", _lecsicalSymbolType, _symbol);
+                }
+                else if (char.IsDigit(_symbol))
+                {
+                    _lecsicalSymbolType = SymbolType.integer;
+                    GetIdentifier();
+                    Console.WriteLine("{0}: {1}", _lecsicalSymbolType, _identifier);
+                }
+                else if (char.IsLetter(_symbol))
+                {
+                    _lecsicalSymbolType = SymbolType.identifier;
+                    GetIdentifier();
+                    Console.WriteLine("{0}: {1}", _lecsicalSymbolType, _identifier);
+                }
+
+
             }
         }
 
         static void Main(string[] args)
         {
+            Program prog = new Program();
+            prog.ReadFromFile("new.txt");
         }
     }
 }
